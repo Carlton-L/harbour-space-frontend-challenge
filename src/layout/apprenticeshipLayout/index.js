da2285836a2c4be494b3852712647e9a31efc81d
Carlton Lindsay <carlton.l@outlook.com>
2022-05-14 13:29:48 -0700

    Fixed box-sizing

--- src/layout/apprenticeshipLayout/index.js
+++ src/layout/apprenticeshipLayout/index.js
@@ -7,8 +7,12 @@ const ApprenticeshipLayout = ({ children }) => {
     <>
       <Global
         styles={css`
+          * {
+            box-sizing: border-box;
+          }
           body {
             margin: 0;
+            font-family: sans-serif;
           }
         `}
       />
