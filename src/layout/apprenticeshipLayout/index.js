c579faca8ec99ec4c7b227399ab9040a3ef2c52d
Carlton Lindsay <carlton.l@outlook.com>
2022-05-14 12:09:33 -0700

    Added global css to remove margin from body

--- src/layout/apprenticeshipLayout/index.js
+++ src/layout/apprenticeshipLayout/index.js
@@ -1,8 +1,16 @@
 import * as React from "react";
+import { Global, css } from "@emotion/react";
 
 const ApprenticeshipLayout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
       <main>{children}</main>
     </>
   );
