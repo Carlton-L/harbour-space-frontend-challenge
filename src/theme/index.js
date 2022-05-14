0b1e62e1628444d6e864988bb541812d1fcfb140
Carlton Lindsay <carlton.l@outlook.com>
2022-05-14 11:51:45 -0700

    Created theme file
    Added color palette, breakpoints, and radii

---
+++ src/theme/index.js
@@ -0,0 +1,14 @@
+// Theme Specification: https://theme-ui.com/theme-spec/
+// NOTE: gatsby-config.js does not support ES6 modules, so you should use module.exports
+module.exports = {
+  colors: {
+    background: "#ffffff",
+    text: "#535353",
+    primary: "#685dc5",
+    secondary: "#4fa16c",
+    accent: "#fbfbfb",
+    muted: "#dadada",
+  },
+  breakpoints: ["23.4375em", "48em", "63em", "64em", "71.125em"],
+  radii: [0, 4, 29],
+};
